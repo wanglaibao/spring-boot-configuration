a056be072a09faaf9df66f19fd042e5592045700
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-01-15 22:32:10 +0800

    add new java files info

---
+++ src/main/java/com/laibao/springboot/springbootconfiguration/SpringBootConfigurationApplication.java
@@ -0,0 +1,17 @@
+package com.laibao.springboot.springbootconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author laibao wang
+ * @date 2018-01-15
+ * @version 1.0
+ */
+@SpringBootApplication
+public class SpringBootConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfigurationApplication.class, args);
+	}
+}
